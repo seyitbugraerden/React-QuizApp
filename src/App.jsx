ff28f324e2f58707f70baf0156513a460c0ed4a0
Bugra <seyitbugraerden@gmail.com>
2024-02-03 02:22:40 +0300

    FieldSet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useContext } from "react";
-import "./App.css";
+import { useContext, useEffect, useState } from "react";
 import { ProductsContext } from "./context/dataContext";
+import { Fieldset } from "primereact/fieldset";
 import Particles from "./components/particles/Particles";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./App.css";
 
 function App() {
   const { items } = useContext(ProductsContext);
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
-      {items[0].question}
+      <Fieldset
+        data-aos="zoom-in"
+        data-aos-delay="200"
+        data-aos-duration="1000"
+        data-aos-mirror="true"
+      >
+        <p className="m-0">{items[0].question}</p>
+      </Fieldset>
+
       <Particles id="tsparticles" />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ProductsProvider } from "./context/dataContext.jsx";
+import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ProductsProvider>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </ProductsProvider>
 );
