4a16f0b7e43ae7f239b9ff3321b273ee020b513b
Bugra <seyitbugraerden@gmail.com>
2024-02-03 20:16:51 +0300

    css updates

--- src/components/questions/questionItem.jsx
+++ src/components/questions/questionItem.jsx
@@ -1,22 +1,51 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ProductsContext } from "../../context/dataContext";
 import ProgressBar from "../progressbar/progressBar";
 
-function questionItem() {
+function QuestionItem() {
   const { items } = useContext(ProductsContext);
+  const [activeOption, setActiveOption] = useState("");
+
+  const handleOptionClick = (option) => {
+    setActiveOption(option);
+  };
+
+  const isActive = (option) => {
+    return activeOption === option;
+  };
+
   return (
     <>
-      <p>{items[0].question}</p>
-
+      <p>{items[2].question}</p>
       <ProgressBar />
       <div className="menu">
-        <div>A-) {items[0].A}</div>
-        <div>B-) {items[0].B} </div>
-        <div>C-) {items[0].C}</div>
-        <div>D-) {items[0].D}</div>
+        <div
+          className={isActive("A") ? "active" : ""}
+          onClick={() => handleOptionClick("A")}
+        >
+          A-) {items[2].A}
+        </div>
+        <div
+          className={isActive("B") ? "active" : ""}
+          onClick={() => handleOptionClick("B")}
+        >
+          B-) {items[2].B}{" "}
+        </div>
+        <div
+          className={isActive("C") ? "active" : ""}
+          onClick={() => handleOptionClick("C")}
+        >
+          C-) {items[2].C}
+        </div>
+        <div
+          className={isActive("D") ? "active" : ""}
+          onClick={() => handleOptionClick("D")}
+        >
+          D-) {items[2].D}
+        </div>
       </div>
     </>
   );
 }
 
-export default questionItem;
+export default QuestionItem;
